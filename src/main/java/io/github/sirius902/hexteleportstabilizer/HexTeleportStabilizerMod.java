8b330d8cb816c08cf14bffa4eefead45cfaa88e5
Sirius902 <10891979+Sirius902@users.noreply.github.com>
2025-07-07 20:58:41 -0700

    init

---
+++ src/main/java/io/github/sirius902/hexteleportstabilizer/HexTeleportStabilizerMod.java
@@ -0,0 +1,55 @@
+package io.github.sirius902.hexteleportstabilizer;
+
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(HexTeleportStabilizerMod.MODID)
+public class HexTeleportStabilizerMod {
+    public static final String MODID = "hex_teleport_stabilizer";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    public static final DeferredBlock<Block> BLOCK_HEX_TELEPORT_STABILIZER = BLOCKS.registerSimpleBlock("hex_teleport_stabilizer", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    public static final DeferredItem<BlockItem> ITEM_HEX_TELEPORT_STABILIZER = ITEMS.registerSimpleBlockItem("hex_teleport_stabilizer", BLOCK_HEX_TELEPORT_STABILIZER, new Item.Properties());
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> HEX_TELEPORT_STABILIZER_TAB = CREATIVE_MODE_TABS.register("hex_teleport_stabilizer_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.hex_teleport_stabilizer"))
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> ITEM_HEX_TELEPORT_STABILIZER.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(BLOCK_HEX_TELEPORT_STABILIZER.get());
+            }).build());
+
+    public HexTeleportStabilizerMod(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        BLOCKS.register(modEventBus);
+        ITEMS.register(modEventBus);
+        CREATIVE_MODE_TABS.register(modEventBus);
+    }
+
+    private void commonSetup(FMLCommonSetupEvent event) {
+    }
+}
